b9fbdc39882a0bfff77e1900ab1193f9e1e7a111
Cleiton Almeida <cleitonant@gmail.com>
2018-09-13 00:49:24 -0300

    First version of models. Call Model

---
+++ app/models.py
@@ -0,0 +1,36 @@
+# app/models.py
+
+from app import db
+
+class Call(db.Model):
+
+    __tablename__ = 'calls'
+
+    id = db.Column(db.Integer, primary_key=True)
+    initial_timestamp = db.Column(db.DateTime(timezone=True))
+    end_timestamp = db.Column(db.DateTime(timezone=True))
+    call_id = db.Column(db.Integer, unique=True)
+    source_number = db.Column(db.String(15))
+    destination_number = db.Column(db.String(15))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+
+    def __init__(self, call_id):
+        self.call_id = call_id
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Call.query.all()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "<Call: {}>".format(self.call_id)
