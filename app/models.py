dc048132f8f9766ab680a7b14c20ad8f8ca910f2
Cleiton Almeida <cleitonant@gmail.com>
2018-09-23 14:22:48 -0300

    Add model Bill

--- app/models.py
+++ app/models.py
@@ -34,3 +34,32 @@ class Call(db.Model):
 
     def __repr__(self):
         return "<Call: {}>".format(self.call_id)
+
+class Bill(db.Model):
+
+    __tablename__ = 'bills'
+
+    id = db.Column(db.Integer, primary_key=True)
+    bill_id = db.Column(db.Integer, unique=True)
+    initial_period = db.Column(db.DateTime(timezone=True))
+    end_period = db.Column(db.DateTime(timezone=True))
+    subscriber_number = db.Column(db.String(15))
+    price = db.Column(db.Float())
+
+    def __init__(self, call_id):
+        self.bill_id = bill_id
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Bill.query.all()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "<Bill: {}>".format(self.bill_id)
