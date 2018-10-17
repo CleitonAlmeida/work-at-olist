7b089d10531305c580e8aec90f2dc90da022218f
Cleiton Almeida <cleitonant@gmail.com>
2018-10-17 00:58:55 -0300

    Fix Model User to username be unique/hash_password

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 # app/models.py
 
 from app import db
+from passlib.hash import pbkdf2_sha256
+from passlib import pwd
 
 class Call(db.Model):
 
@@ -67,5 +69,25 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(32), index = True)
+    username = db.Column(db.String(32), index = True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    def hash_password(self, password):
+        self.password_hash = pbkdf2_sha256.hash(password)
+
+    def verify_password(self, password):
+        return pbkdf2_sha256.verify(password, self.password_hash)
+
+    def generate_password(self, length):
+        return pwd.genword(length=length)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "<User: {}>".format(self.username)
