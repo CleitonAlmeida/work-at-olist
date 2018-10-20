9b5a743689a489a7296bc5494fd427cd9f91b334
Cleiton Almeida <cleitonant@gmail.com>
2018-10-20 02:34:05 -0300

    Fix run.py to use HOST/PORT env var

--- run.py
+++ run.py
@@ -6,4 +6,7 @@ config_name = os.getenv('FLASK_ENV')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    host = os.environ.get('HOST', '0.0.0.0')
+    app.run(host=host, port=port)
