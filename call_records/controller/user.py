0af97c8d10397a26605909bb87d5d159131d2aea
Cleiton Almeida <cleitonant@gmail.com>
2019-01-27 23:52:10 -0200

    Add some msgs to fixed conf

--- call_records/controller/user.py
+++ call_records/controller/user.py
@@ -1,9 +1,10 @@
 from flask import current_app
 from flask_restplus import Resource
 
+from call_records import fixed
 from call_records.dto.user import UserDto
 from call_records.service.user import (save_user, get_a_user, get_all_users,
-    update_user)
+                                       update_user)
 from call_records.controller import user_required, admin_required
 from flask_restplus import reqparse, inputs
 
@@ -12,23 +13,31 @@ userDtoModel = UserDto.user
 userListDtoModel = UserDto.userList
 userResponseDtoModel = UserDto.userResponses
 
+
 def get_parser_user():
     parser = reqparse.RequestParser()
-    parser.add_argument('username', type=str, required=True)
-    parser.add_argument('password', type=str, required=True)
+    parser.add_argument('username', type=str, required=True, help='Username')
+    parser.add_argument('password', type=str, required=True, help='Password')
     return parser
+
 
 def get_parser_update_user():
     parser = reqparse.RequestParser()
-    parser.add_argument('is_admin', type=inputs.boolean, required=False, help='The role of user (if admin or not admin)')
-    parser.add_argument('password', type=str, required=True)
+    parser.add_argument('is_admin', type=inputs.boolean, required=False,
+                        help='The role of user (if admin or not admin)')
+    parser.add_argument('password', type=str,
+                        required=True, help='User password')
     return parser
+
 
 def get_parser_pagination():
     parser = reqparse.RequestParser()
-    parser.add_argument('start', type=int, required=False, default=1)
-    parser.add_argument('limit', type=int, required=False)
+    parser.add_argument('start', type=int, required=False, default=1,
+                        help='The position from which the data will be returned')
+    parser.add_argument('limit', type=int, required=False,
+                        help='The max number of items to return from the current position')
     return parser
+
 
 @ns.route('/')
 class User(Resource):
@@ -39,16 +48,18 @@ class User(Resource):
         """List all registered users"""
         parser = get_parser_pagination()
         data = parser.parse_args()
-        return get_all_users(paginated=True, start=data.get('start'), limit=data.get('limit'))
+        return get_all_users(paginated=True,
+                            start=data.get('start'),
+                            limit=data.get('limit'))
 
     @admin_required
     @ns.expect(get_parser_user(), validate=True)
     @ns.doc(responses={
-        201: 'Successfully registered',
-        409: 'User already exists. Please Log in'
+        201: fixed.MSG_SUCCESSFULLY_REGISTRED,
+        409: fixed.MSG_USER_EXISTING
     })
     def post(self):
-        """Create a new user"""
+        """To create a new user"""
         parser = get_parser_user()
         data = parser.parse_args()
         return save_user(data=data)
@@ -56,7 +67,7 @@ class User(Resource):
 
 @ns.route('/<username>')
 @ns.param('username', 'The User identifier')
-@ns.response(404, 'User not found.')
+@ns.response(404, fixed.MSG_USER_NOT_FOUND)
 class UserSpecific(Resource):
     @admin_required
     @ns.marshal_with(userDtoModel)
@@ -68,7 +79,7 @@ class UserSpecific(Resource):
         """Get an user given its identifier"""
         user = get_a_user(username)
         if not user:
-            ns.abort(404, 'User not found.')
+            ns.abort(404, fixed.MSG_USER_NOT_FOUND)
         else:
             return user
 
@@ -76,8 +87,8 @@ class UserSpecific(Resource):
     @ns.expect(get_parser_update_user(), validate=True)
     @ns.marshal_with(userResponseDtoModel, skip_none=True)
     @ns.doc(responses={
-        200: 'Successfully updated',
-        404: 'User not found'
+        200: fixed.MSG_SUCCESSFULLY_UPDATED,
+        404: fixed.MSG_USER_NOT_FOUND
     })
     def put(self, username):
         """To update an user"""
--- call_records/fixed.py
+++ call_records/fixed.py
@@ -3,7 +3,11 @@ MSG_SUCCESSFULLY_UPDATED = 'Successfully updated'
 MSG_ACTION_NOT_ALLOWED = 'Action not allowed'
 MSG_404 = 'Page not found'
 MSG_ONLY_ADMIN = 'You must be admin'
+MSG_SUCCESS_LOGOUT = 'Logout Successfully'
 
 MSG_CALL_NOT_FOUND = 'Call not found'
 MSG_INVALID_PHONE_NUMBER = 'Invalid phone number'
 MSG_CONFLICT_CALLS = 'Connection period conflict'
+
+MSG_USER_NOT_FOUND = 'User not found'
+MSG_USER_EXISTING = 'User already exists. Please Log in'
